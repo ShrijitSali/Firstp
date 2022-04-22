6c043ae044a84634e0be1614c3c772419fbd38fc
Shrijit <Shrijitsali@gmail.com>
2022-04-22 23:18:53 +0530

    Initial commit

---
+++ nested_lists.py
@@ -0,0 +1,28 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam", "spam"],
+]
+#
+# for meal in menu:
+#     if "spam" not in meal:  #those list that will not contain spam
+#         print(meal)
+#
+#         for item in meal:
+#             print(item)    #only selected above will be used or processed further
+#
+#     else:
+#        print(f"{meal} has {meal.count('spam')} spam") #count will directly count the occurances of word in bracket
+
+# remove spam word
+for meal in menu:
+    for index in range(len(meal) - 1,-1,-1):
+        # print(index,word)
+        if meal[index] == "spam":
+            del meal[index]
+    print(meal)
